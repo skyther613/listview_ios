96d77c344f4c90f9d75546ffc3d2fd7b086a7537
djkam <djkam613@gmail.com>
2021-07-14 08:55:37 +0900

    Initial Commit

---
+++ listview_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  listview_ios
+//
+//  Created by Dongjin Kam on 2021/07/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ listview_ios/listview_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  listview_iosApp.swift
+//  listview_ios
+//
+//  Created by Dongjin Kam on 2021/07/14.
+//
+
+import SwiftUI
+
+@main
+struct listview_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
